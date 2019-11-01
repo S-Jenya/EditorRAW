b2c35ca7491179d6fb3b3d65ceeded265272fb0a
S-Jenya <s_j_n@bk.ru>
2019-11-01 21:11:45 +0300

    Edit exposure value and save change.

--- src/ru/oti/editorRAW.java
+++ src/ru/oti/editorRAW.java
@@ -1,41 +1,52 @@
 package ru.oti;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
-
 
 public class editorRAW {
 
-
-
     public static void main(String[] args) throws IOException {
-	// write your code here
+        // write your code here
         String line;
-        int v;
 
         File file = new File("D:\\Project\\Java\\Editor\\src\\ru\\oti\\Files\\test.xmp");
         Scanner scanner = new Scanner(file);
-        FileWriter write = new FileWriter("D:\\Project\\Java\\Editor\\src\\ru\\oti\\Files\\test.xmp");
 
+        File MyFile = new File("D:\\Project\\Java\\Editor\\src\\ru\\oti\\test.xmp");
+        PrintWriter pw = new PrintWriter(MyFile);
+
+        System.out.println("Начала работы. Пожалуйста, подождите...");
 
         while (scanner.hasNextLine()){
             line = scanner.nextLine();
-            String strExposureMode = line.substring(line.indexOf(':')+1, line.indexOf('='));
-            String valueExposureMode = line.substring(line.indexOf('=') + 2,  line.length()-1);
 
-            if("ExposureMode".equals(strExposureMode)){
-                System.out.println("String: " + strExposureMode + "\t" + "Value: " + valueExposureMode);
-                v = Integer.parseInt(valueExposureMode);
-                write.write("exif:ExposureMode=\"-1\"");
-                write.close();
-                System.out.println("Integer value my Exp = : " + v);
-//                break;
+            int end = line.indexOf('=');
+            int start = line.indexOf(':') + 1;
+
+            if(start > 0 && end > 0){
+
+                String strExposure = line.substring(start, end);
+                String valueExposure = line.substring(line.indexOf('=') + 2,  line.length() - 1);
+
+                if("Exposure2012".equals(strExposure)){
+                    // увеличиваем значение экспозиции на 0,5
+                    Double value = Double.parseDouble(valueExposure) + 0.5;
+                    pw.print("   crs:Exposure2012=\"" + value +"\"\n");
+                } else {
+                    pw.println(line);
+                }
+            } else {
+                pw.println(line);
             }
-
-            write.write("line");
 
         }
 
+        pw.close();
         scanner.close();
+
+        System.out.println("Результат работы программы записан в test.xmp");
+
     }
 }
